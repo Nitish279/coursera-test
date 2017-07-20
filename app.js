606cf9fa73f775dccb9acd975dfa815116dbd240
Nitish Anand <nitish4u029@gmail.com>
2017-07-21 04:32:30 +0530

    check add if no input is provided by user

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-(function () {
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('firstAssignment', [])
-	.controller('firstAssignmentCtrl', function ($scope) {
-		$scope.getItemsName = function(){
-			var itemsList = $scope.foodItems.split(/\s*,\s*/);
-			if (itemsList.length <= 3) {
-				$scope.returnMsg = "Enjoy";
-			} else {
-				$scope.returnMsg = "Too much!";
-			}
-		}
-	});
-	
+    angular.module('firstAssignment', [])
+        .controller('firstAssignmentCtrl', function($scope) {
+
+            $scope.foodItems = '';
+            $scope.checkContent = function() {
+
+                if ($scope.foodItems.length === 0 || typeof $scope.foodItems === 'undefined') {
+                    $scope.returnMsg = "Please enter data first";
+                } else {
+                    var itemsList = $scope.foodItems.split(/\s*,\s*/);
+                    if (itemsList.length <= 3) {
+                        $scope.returnMsg = "Enjoy";
+                    } else {
+                        $scope.returnMsg = "Too much!";
+                    }
+                }
+
+            }
+
+        });
+
 })();
